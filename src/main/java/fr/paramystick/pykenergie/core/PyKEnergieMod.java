f630e17744c46e6d342853a2d1e8afa1b8c454c5
BlackDrag00n <blackdragoonsoad@gmail.com>
2014-06-18 20:48:21 +0200

    Réparation de conflit
    Réparation de conflit

--- src/main/java/fr/paramystick/pykenergie/core/PyKEnergieMod.java
+++ src/main/java/fr/paramystick/pykenergie/core/PyKEnergieMod.java
@@ -1,14 +1,9 @@
 package fr.paramystick.pykenergie.core;
 
-<<<<<<< HEAD:src/main/java/fr/paramystick/pykenergie/core/PyKEnergieMod.java
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-=======
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
->>>>>>> origin/master:src/main/java/fr/paramystick/energie/core/Energie.java
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -16,13 +11,9 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-<<<<<<< HEAD:src/main/java/fr/paramystick/pykenergie/core/PyKEnergieMod.java
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import fr.paramystick.pykenergie.gui.PyKEnergieModTabs;
-=======
 import cpw.mods.fml.common.registry.GameRegistry;
-import fr.paramystick.energie.items.ItemGourde;
->>>>>>> origin/master:src/main/java/fr/paramystick/energie/core/Energie.java
+import fr.paramystick.pykenergie.items.ItemGourde;
 
 @Mod(modid = "pykenergiemod", name = "PyK Energie Mod", version = "0.0.1", dependencies = "required-after:ffmtlibs")
 public class PyKEnergieMod
@@ -30,16 +21,10 @@ public class PyKEnergieMod
 	/**
 	 * L'instance du mod
 	 */
-<<<<<<< HEAD:src/main/java/fr/paramystick/pykenergie/core/PyKEnergieMod.java
 	@Instance("pykenergiemod")
 	public static PyKEnergieMod instance;
     public static final String MODID = "pykenergiemod";
-=======
-	@Instance("modenergie")
-	public static Energie instance;
-    public static final String MODID = "modenergie";
     public static Item itemGourde; // Gourde
->>>>>>> origin/master:src/main/java/fr/paramystick/energie/core/Energie.java
     
     /**
      * Déclaration des proxy, on appel cette méthode dans la méthode init
--- src/main/java/fr/paramystick/pykenergie/items/ItemGourde.java
+++ src/main/java/fr/paramystick/pykenergie/items/ItemGourde.java
@@ -1,18 +1,14 @@
-package fr.paramystick.energie.items;
+package fr.paramystick.pykenergie.items;
 
-import fr.paramystick.energie.core.Energie;
+import fr.paramystick.pykenergie.core.Energie;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
 
 public class ItemGourde extends Item
 {
-	
 	@Override
 	public void registerIcons(IIconRegister iconregister)
 	{
-		
-		this.itemIcon = iconregister.registerIcon(Energie.MODID + ":itemgourde");
-
+		this.itemIcon = iconregister.registerIcon(PyKEnergieMod.MODID + ":itemgourde");
 	}
-	
 }
